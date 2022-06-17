82bd4a076015db19885b1926cbe37d0f831bbea3
Yan <yangabriel2012@gmail.com>
2022-06-17 12:04:18 -0300

    commit respondive gallery

---
+++ src/App.js
@@ -0,0 +1,66 @@
+
+import './App.css';
+import new2 from './comp/new2.jpeg'
+import new1 from './comp/new1.jpeg'
+import new3 from './comp/new3.jpeg'
+import new4 from './comp/new4.jpeg'
+import new5 from './comp/new5.jpeg'
+import new6 from './comp/new6.jpeg'
+import new7 from './comp/new7.jpeg'
+import new8 from './comp/new8.jpeg'
+import new9 from './comp/new9.jpeg'
+import new10 from './comp/new10.jpeg'
+import new11 from './comp/new11.jpeg'
+import show from './comp/newfinal.jpeg'
+import new13 from './comp/new13.jpeg'
+import sula1 from './comp/newsula1.jpeg'
+import park1 from './comp/newpacinha1.jpeg'
+import park2 from './comp/newpracinha2.jpeg'
+import park3 from './comp/newpracinha3.jpeg'
+import bar from  './comp/redimFinal1.jpeg'
+import bear from  './comp/newFinal2.jpeg'
+import new12 from './comp/new12.jpeg'
+
+
+
+function App() {
+  return (
+    <div>
+      <div>
+       <h1 className='display-1'>Responsive gallery</h1>
+      </div>
+<div className='col-12 col-sm-6 col-md-4 col-lg-2 col-xl-2'>
+      <div className='App'>
+      <div><img src={new11}/></div>
+      <div><img src={new2}/></div>
+      <div><img src={new3}/></div>
+      <div><img src={new4}/></div>
+      <div><img src={new5}/></div>
+      <div><img src={new6}/></div>
+      <div><img src={new7}/></div>
+      <div><img src={new8}/></div>
+      <div><img src={new9}/></div>
+      <div><img src={new10}/></div>
+      <div><img src={sula1}/></div>
+      <div><img src={new12}/></div>
+      <div><img src={new13}/></div>
+      <div><img src={park2}/></div>
+      <div><img src={park1}/></div>
+      <div><img src={park3}/></div>
+      <div><img src={new1}/></div>
+      <div><img src={bear}/></div>
+      <div><img src={bar}/></div>
+      <div><img src={show}/></div>
+    </div>
+    </div>
+    </div>
+    
+    
+    
+  
+  )
+    
+
+}
+
+export default App;
